e5308a80fef0d4c55e4eb73aeb4e6810521b6612
pedernil <pedernil@gmail.com>
2020-02-11 12:30:58 +0100

    Tryout prep for GoogleMaps

--- FallDetectionApp/FallDetectionApp.Android/MainActivity.cs
+++ FallDetectionApp/FallDetectionApp.Android/MainActivity.cs
@@ -6,12 +6,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Content;
+using Xamarin.Forms.PlatformConfiguration;
+using Android;
+using Android.Util;
+
 
 namespace FallDetectionApp.Droid
 {
+
+
     [Activity(Label = "FallDetectionApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private readonly string TAG = "Log MainActivity";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -23,11 +32,50 @@ namespace FallDetectionApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+        /*
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+        */
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (true) //requestCode == REQUEST_LOCATION
+            {
+                // Received permission result for camera permission.
+                Log.Info(TAG, "Received response for Location permission request.");
+
+                // Check if the only required permission has been granted
+                if ((grantResults.Length == 1) && (grantResults[0] == Permission.Granted))
+                {
+                    // Location permission has been granted, okay to retrieve the location of the device.
+                    Log.Info(TAG, "Location permission has now been granted.");
+                    // Snackbar.Make(layout, Resource.String.permission_available_camera, Snackbar.LengthShort).Show();
+                }
+                else
+                {
+                    Log.Info(TAG, "Location permission was NOT granted.");
+                    //Snackbar.Make(layout, Resource.String.permissions_not_granted, Snackbar.LengthShort).Show();
+                }
+            }
+            else
+            {
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
+
+
+
+
+
+
 }
