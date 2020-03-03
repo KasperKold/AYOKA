47e498ae3d18942e7e0987677030445607fec3a2
Kasper Kold Pedersen <kkp@comit.dk>
2020-03-03 11:20:50 +0100

    Changed name of ItemsPage to ConfigPage once and for all.

--- FallDetectionApp/FallDetectionApp/Views/ConfigPage.xaml.cs
+++ FallDetectionApp/FallDetectionApp/Views/ConfigPage.xaml.cs
@@ -16,11 +16,11 @@ namespace FallDetectionApp.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class ItemsPage : ContentPage
+    public partial class ConfigPage : ContentPage
     {
         ConfigViewModel viewModel;
 
-        public ItemsPage()
+        public ConfigPage()
         {
             InitializeComponent();
 
