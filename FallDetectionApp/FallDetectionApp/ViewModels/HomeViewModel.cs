cc9cb34ed7944e138e437fb77cc733345eb3cd1d
Kasper Kold Pedersen <kkp@comit.dk>
2020-03-05 13:02:41 +0100

    nothing significant

--- FallDetectionApp/FallDetectionApp/ViewModels/HomeViewModel.cs
+++ FallDetectionApp/FallDetectionApp/ViewModels/HomeViewModel.cs
@@ -20,40 +20,5 @@ namespace FallDetectionApp.ViewModels
         public ICommand OpenWebCommand { get; }
         public ICommand GetGeoLocation { get; }
 
-
-
-
-
-
-        //public async Task<bool> GetGeoLocationAsync()
-        //{
-        //    try
-        //    {
-        //        var location = await Geolocation.GetLocationAsync();
-
-        //        if (location != null)
-        //        {
-        //            Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
-        //        }
-        //    }
-        //    catch (FeatureNotSupportedException fnsEx)
-        //    {
-        //        // Handle not supported on device exception
-        //    }
-        //    catch (FeatureNotEnabledException fneEx)
-        //    {
-        //        // Handle not enabled on device exception
-        //    }
-        //    catch (PermissionException pEx)
-        //    {
-        //        // Handle permission exception
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Unable to get location
-        //    }
-
-        //    return await Task.FromResult(true);
-        //}
     }
 }
