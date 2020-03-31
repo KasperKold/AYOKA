1bdba989554e2795527f89c000bc14e1d9414a7e
pedernil <pedernil@gmail.com>
2020-03-31 10:38:57 +0200

    added missing classes to git

---
+++ FallDetectionApp/FallDetectionApp/Services/IUpdateGeo.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FallDetectionApp.Services
+{
+    public interface IUpdateGeo
+    {
+        void updateGeo();
+        //Task<bool> UpdateItemAsync(T item);
+        //Task<bool> DeleteItemAsync(string id);
+        //Task<T> GetItemAsync(string id);
+        //Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
