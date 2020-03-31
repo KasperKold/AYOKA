eafaa10127dd27cc8e99d82c05c896beec8a00dd
pedernil <pedernil@gmail.com>
2020-03-31 10:41:07 +0200

    added missing IUihandler

---
+++ FallDetectionApp/FallDetectionApp/Services/IUiHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FallDetectionApp.Models;
+
+namespace FallDetectionApp.Services
+{
+    public interface IUiHandler
+    {
+        Task<GeoLocation> GeoUpdateAsync();
+        //Task<bool> syncGeo();
+        //Task<bool> DeleteItemAsync(string id);
+        //Task<T> GetItemAsync(string id);
+        //Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
